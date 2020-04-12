e071ef27e0a588a04479e11a2c45bd85039f8673
alexcol23-casa <alexander_col23@hotmail.com>
2020-04-11 23:09:47 -0500

    Input output elements

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,23 +5,34 @@ import {Graficas1Component} from './graficas1/graficas1.component';
 import {PagesComponent} from './pages.component';
 import {SharedModule} from '../shared/shared.module';
 import {PAGES_ROUTES} from './pages.routes';
+import {FormsModule} from '@angular/forms';
+import {IncrementadorComponent} from '../components/incrementador/incrementador.component';
+import {ChartsModule} from 'ng2-charts';
+import {GraficoDonaComponent} from '../components/grafico-dona/grafico-dona.component';
+import {CommonModule} from '@angular/common';
+
 
 @NgModule({
-  declarations: [
-    DashboardComponent ,
-    ProgressComponent ,
-    Graficas1Component ,
-    PagesComponent
-  ] ,
-  exports: [
-    DashboardComponent ,
-    ProgressComponent ,
-    Graficas1Component ,
-    PagesComponent
-  ] ,
+    declarations: [
+        DashboardComponent ,
+        ProgressComponent ,
+        Graficas1Component ,
+        PagesComponent ,
+        IncrementadorComponent ,
+        GraficoDonaComponent
+    ] ,
+    exports: [
+        DashboardComponent ,
+        ProgressComponent ,
+        Graficas1Component ,
+        PagesComponent
+    ] ,
   imports: [
-    SharedModule,
-    PAGES_ROUTES
+    SharedModule ,
+    PAGES_ROUTES ,
+    FormsModule ,
+    ChartsModule ,
+    CommonModule
   ]
 })
 export class PagesModule {
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component , OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-progress',
-  templateUrl: './progress.component.html',
+  selector: 'app-progress' ,
+  templateUrl: './progress.component.html' ,
   styles: []
 })
 export class ProgressComponent implements OnInit {
 
-  constructor() { }
+  progreso1 : number = 20;
+  progreso2 : number = 30;
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+  ngOnInit() : void {
   }
 
 }
